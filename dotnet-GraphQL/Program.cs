b50227ccb1b243143ef2eb97bb964b0b7c4b2dbc
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-07 20:40:17 +0100

    creating dbcontext

--- dotnet-GraphQL/Program.cs
+++ dotnet-GraphQL/Program.cs
@@ -1,3 +1,6 @@
+using dotnet_GraphQL.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opts => opts.UseInMemoryDatabase("BlogsManagement"));
 
 var app = builder.Build();
 
