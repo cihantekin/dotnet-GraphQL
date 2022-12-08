117af476597e5bb02c22f2c4808cd5ac235f81d0
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-08 22:42:41 +0100

    graphQL type classes

--- dotnet-GraphQL/GraphQL/AuthorType.cs
+++ dotnet-GraphQL/GraphQL/AuthorType.cs
@@ -1,6 +1,14 @@
-﻿namespace dotnet_GraphQL.GraphQL
+﻿using dotnet_GraphQL.DataAccess.Models;
+
+namespace dotnet_GraphQL.GraphQL
 {
-    public class AuthorType
+    public class AuthorType : ObjectType<Author>
     {
+        protected override void Configure(IObjectTypeDescriptor<Author> descriptor)
+        {
+            descriptor.Field(f => f.Id).Type<IdType>();
+            descriptor.Field(f => f.FirstName).Type<StringType>();
+            descriptor.Field(f => f.LastName).Type<StringType>();
+        }
     }
 }
--- dotnet-GraphQL/GraphQL/BlogPostType.cs
+++ dotnet-GraphQL/GraphQL/BlogPostType.cs
@@ -1,6 +1,14 @@
-﻿namespace dotnet_GraphQL.GraphQL
+﻿using dotnet_GraphQL.DataAccess.Models;
+
+namespace dotnet_GraphQL.GraphQL
 {
-    public class BlogPostType
+    public class BlogPostType : ObjectType<BlogPost>
     {
+        protected override void Configure(IObjectTypeDescriptor<BlogPost> descriptor)
+        {
+            descriptor.Field(f => f.Id).Type<IdType>();
+            descriptor.Field(f => f.Title).Type<StringType>();
+            descriptor.Field(f => f.AuthorId).Type<IdType>();
+        }
     }
 }
