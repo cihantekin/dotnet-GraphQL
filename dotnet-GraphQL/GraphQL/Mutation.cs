9c165b64dc87ed1408512ea7745925fc3d8835be
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-10 19:12:40 +0100

    mutation class creates a new author

--- dotnet-GraphQL/GraphQL/Mutation.cs
+++ dotnet-GraphQL/GraphQL/Mutation.cs
@@ -1,7 +1,25 @@
-﻿namespace dotnet_GraphQL.GraphQL
+﻿using dotnet_GraphQL.DataAccess;
+using dotnet_GraphQL.DataAccess.Models;
+using HotChocolate.Subscriptions;
+
+namespace dotnet_GraphQL.GraphQL
 {
     //This class is to allow clients to add, remove or modify.
     public class Mutation
     {
+        public async Task<Author> CreateAuthor([Service] IAuthorRepository authorRepository, [Service] ITopicEventSender eventSender, int id, string firstName, string lastName)
+        {
+            Author author = new ()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                Id = id
+            };
+
+            var result = await authorRepository.Create(author);
+            await eventSender.SendAsync("AuthorCreated", result);
+
+            return result;
+        }
     }
 }
