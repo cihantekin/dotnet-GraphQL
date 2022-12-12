4e5ea283e369508d5b8ab7c06ab3bcfca87430a2
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-12 23:06:06 +0100

    AuthorResolver

---
+++ dotnet-GraphQL/GraphQL/AuthorResolver.cs
@@ -0,0 +1,22 @@
+﻿using dotnet_GraphQL.DataAccess;
+using dotnet_GraphQL.DataAccess.Models;
+using HotChocolate.Resolvers;
+
+namespace dotnet_GraphQL.GraphQL
+{
+    // this class is value of a type in a schema
+    public class AuthorResolver
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorResolver(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public Author GetAuthor(BlogPost blog, IResolverContext resolverContext)
+        {
+            return _authorRepository.GetList().Where(s => s.Id == blog.AuthorId).FirstOrDefault();
+        }
+    }
+}
